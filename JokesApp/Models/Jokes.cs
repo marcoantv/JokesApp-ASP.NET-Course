53bf8a87634f1bb81be937bbd25b1ac6742f2cd2
Marco Viquez <maviquez1@gmail.com>
2022-11-12 00:28:35 -0600

    Add project files.

---
+++ JokesApp/Models/Jokes.cs
@@ -0,0 +1,16 @@
+﻿namespace JokesApp.Models
+{
+    public class Jokes
+    {
+        public int Id { get; set; }
+
+        public string JokeQuestion { get; set; }
+
+        public string JokeAnswer { get; set; }
+
+        public Jokes()
+        {
+
+        }
+    }
+}
